5f67eb95b4cbfd8cdf66e0d88509c42a4e71f814
Samson Cheung <samsonc89@yahoo.com>
2023-01-13 16:07:47 -0800

    Add function to separate weather details

--- script.js
+++ script.js
@@ -7,13 +7,14 @@ const currentTime = document.querySelector("#current-time");
 const tempNumber = document.querySelector("#temp-number");
 const highTemp = document.querySelector("#high-temp");
 const lowTemp = document.querySelector("#low-temp");
+const iconImage = document.querySelector("#icon-image");
 const weatherDescription = document.querySelector("#weather-description");
 const APIkey = "5943b09bd72402e9560b276898d410f0";
 
 //When search button is clicked
 
 searchBtn.addEventListener("click", () => {
-  updateWeatherDisplay();
+  updateCurrentWeatherDisplay();
 });
 
 searchInput.value = "Oakland";
@@ -33,33 +34,57 @@ async function getLatAndLon() {
   return cityCoordinates;
 }
 
-async function getWeatherData() {
+async function getAllWeatherData() {
   const coordinates = await getLatAndLon();
   const response = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?lat=${coordinates[0]}&lon=${coordinates[1]}&appid=${APIkey}`,
+    `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates[0]}&lon=${coordinates[1]}&appid=${APIkey}`,
     { mode: "cors" }
   );
-
   const data = await response.json();
   console.log(data);
   return data;
 }
 
-async function updateWeatherDisplay() {
-  const data = await getWeatherData();
+async function getCurrentWeatherData() {
+  const response = await getAllWeatherData();
+  const data = await response.current;
+  console.log(data);
+  return data;
+}
 
-  cityName.textContent = data.name; //change to user input
+async function updateWeatherDetails(data) {
+  let newData = {
+    sunrise: data.sunrise,
+    sunset: data.sunset,
+    feels_like: data.feels_like,
+    wind_speed: data.wind_speed,
+    wind_deg: data.wind_deg,
+    humidity: data.humidity,
+    uvi: data.uvi,
+  };
+
+  console.log(newData);
+  return newData;
+}
+
+async function updateCurrentWeatherDisplay() {
+  const currentData = await getCurrentWeatherData();
+  updateWeatherDetails(currentData);
+
+  cityName.textContent = currentData.name; //change to user input
 
   //Capitalize first letter of the description
   let weatherName =
-    data.weather[0].description.charAt(0).toUpperCase() +
-    data.weather[0].description.substring(1);
-
+    currentData.weather[0].description.charAt(0).toUpperCase() +
+    currentData.weather[0].description.substring(1);
+  iconImage.src = `http://openweathermap.org/img/wn/${currentData.weather[0].icon}@2x.png`;
   weatherDescription.textContent = weatherName;
-  tempNumber.textContent = Math.round(data.main.temp * (9 / 5) - 459.67);
-  highTemp.textContent = Math.round(data.main.temp_max * (9 / 5) - 459.67);
-  lowTemp.textContent = Math.round(data.main.temp_min * (9 / 5) - 459.67);
+  tempNumber.textContent = Math.round(currentData.temp * (9 / 5) - 459.67);
+  //move this to details
+  //   highTemp.textContent = Math.round(data.main.temp_max * (9 / 5) - 459.67);
+  //   lowTemp.textContent = Math.round(data.main.temp_min * (9 / 5) - 459.67);
 }
+
 /*http://api.openweathermap.org/geo/1.0/direct?q=
 {city name},{state code},{country code}&limit={limit}&appid={API key}
 
@@ -76,6 +101,9 @@ onst response = await fetch("./city.list.json");
   console.log(newCityList);
 
 Lookup later: debouncing to make autocomplete wait some milliseconds before starting the search
+
+
+
 
 
 */
